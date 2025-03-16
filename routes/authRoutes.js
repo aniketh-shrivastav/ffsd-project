9ef947db8232d7a8daa4cc5dc614f97f7c261312
Aniketh <anishrivastav24@gmail.com>
2025-03-16 12:32:32 +0530

    included partials

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -108,6 +108,10 @@ router.get("/contactus", (req, res) =>{
 router.get("/feedback", (req, res) =>{
   res.render("all/feedback", {user: req.session.user});
 });
+router.get("/faq", (req, res) => {
+  console.log("✅ /faq route hit");
+  res.render("all/faq", { user: req.session.user });
+});
 
 // Logout
 router.get("/logout", (req, res) => {
